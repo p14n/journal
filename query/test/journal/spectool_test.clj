1c53b0e8f90a57c4da8b64475281b5e4cbb06bff
Dean Chapman <dean@p14n.com>
2018-01-09 08:42:52 +0000

    More tests

--- query/test/journal/spectool_test.clj
+++ query/test/journal/spectool_test.clj
@@ -51,47 +51,65 @@
   (read-string
    (g/read-edn-from-file "test-resources/target-schema.edn")))
 
-(def converted (pst/convert-to-object-tuples app-schema))
-
-(def datomic-schema (pst/create-datomic-schema converted))
-
-;;(pprint datomic-schema)
-
 (defn type-mapping-func [field]
   (let [match (if (= field '::ID) (symbol "ID") nil)]
     match))
 
-(def graphql (pst/convert-to-graphql
-              converted
-              type-mapping-func
-              '::ID))
-;;[::addGroupToPerson]
-;;(pprint graphql)
-
 (deftest graphql-conversion
   (testing "Graphql conversion"
-    (testing "to objects"
-      (is (= (:objects schema-map) (:objects graphql))))
-    (testing "to queries"
-      (is (= (:queries schema-map) (:queries graphql))))
-    (testing "to mutations"
-      (is (= (:mutations schema-map) (:mutations graphql))))
-    (testing "mutations from function spec"
-      (let [converted (gst/mutation-from-spec ::addGroupToPerson type-mapping-func)]
-        (is (= [:addGroupToPerson
-                {:args
-                 {:person {:type 'ID}
-                  :group {:type 'ID}}
-                 :resolve :mutation/addGroupToPerson
-                 :type :Person}] converted))))))
+    (let [converted (pst/convert-to-object-tuples app-schema)
+          graphql (pst/convert-to-graphql
+                   converted
+                   type-mapping-func
+                   '::ID)]
+      (testing "to objects"
+        (is (= (:objects schema-map) (:objects graphql))))
+      (testing "to queries"
+        (is (= (:queries schema-map) (:queries graphql))))
+      (testing "to mutations"
+        (is (= (:mutations schema-map) (:mutations graphql))))
+      (testing "mutations from function spec"
+        (let [converted (gst/mutation-from-spec ::addGroupToPerson type-mapping-func)]
+          (is (= [:addGroupToPerson
+                  {:args
+                   {:person {:type 'ID}
+                    :group {:type 'ID}}
+                   :resolve :mutation/addGroupToPerson
+                   :type :Person}] converted)))))))
 
 (deftest datomic-conversion
   (testing "Datomic conversion"
-    (testing "identifies many relationships"
-      (is (dst/is-many '(::Group clojure.spec.alpha/coll-of))))
-    (testing "contains Person and Group"
-      (do (is (= "Person" (first (keys datomic-schema))))
-          (is (= "Group" (second (keys datomic-schema))))))))
+    (let [converted (pst/convert-to-object-tuples app-schema)
+          datomic-schema (pst/create-datomic-schema converted)]
+      (testing "identifies many relationships"
+        (is (dst/is-many '(::Group clojure.spec.alpha/coll-of))))
+      (testing "contains Person and Group"
+        (do (is (= "Person" (first (keys datomic-schema))))
+            (is (= "Group" (second (keys datomic-schema)))))))))
 
+(deftest datomic-field-creation
+  (testing "A datomic field is correctly created"
+    (testing "Creates unique single field"
+      (is (= {:db/cardinality :db.cardinality/one,
+              :db/ident :Person/email,
+              :db/unique :db.unique/identity,
+              :db/valueType :db.type/string,
+              :db.install/_attribute :db.part/db}
+             (dst/create-datomic-field "Person" "email" false "string" true))))
+    (testing "Creates non-unique collection field"
+      (is (= {:db/cardinality :db.cardinality/many,
+              :db/ident :Person/middlenames,
+              :db/valueType :db.type/string,
+              :db.install/_attribute :db.part/db}
+             (dst/create-datomic-field "Person" "middlenames" true "string" false))))))
 
-
+(deftest datomic-types
+  (testing "All datomic types are coverted"
+    (testing "String" (is (= "string"
+                    (dst/datomic-type 'clojure.core/string? {}))))
+    (testing "String in collection" (is (= "string"
+                    (dst/datomic-type ['clojure.core/string?] {}))))
+    (testing "Long"(is (= "long"
+                    (dst/datomic-type 'clojure.core/int? {}))))
+    (testing "Reference" (is (= "ref"
+                    (dst/datomic-type :Person #{:Person}))))))
